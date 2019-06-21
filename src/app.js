021b1664fac02166d865459b8fa6af2c8f751b44
m7Aei <mateuspm2015@hotmail.com>
2019-06-21 11:58:47 -0300

    Fazendo a parte do app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
-const express = require('express');
+const express = require("express");
 
 class AppController {
-  constructor() { 
+  constructor() {
     this.express = express();
 
     this.middlewares();
     this.routes();
-  }  
+  }
 
   middlewares() {
-
+    this.express.use(express.json());
   }
 
   routes() {
-    
+    this.express.use(require("./routes"));
   }
 }
+
+module.exports = new AppController().express;
