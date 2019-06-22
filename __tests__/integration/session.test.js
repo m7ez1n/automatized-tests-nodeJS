2b2736edf5456042e585fa4644198f4dc7834178
m7Aei <mateuspm2015@hotmail.com>
2019-06-22 12:58:23 -0300

    Fazendo os testes de autenticação

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -40,4 +40,40 @@ describe("Authentication", () => {
 
     expect(response.status).toBe(401);
   });
+
+  it("should return jwt token when authentication", async () => {
+    const user = await User.create({
+      name: "Rodrigo",
+      email: "rodrigo@gmail.com",
+      password: "123123"
+    });
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: user.email,
+        password: "123123"
+      });
+
+    expect(response.body).toHaveProperty("token");
+  });
+
+  it("should be able to acess private routes when authentication", async () => {
+    const user = await User.create({
+      name: "Rodrigo",
+      email: "rodrigo@gmail.com",
+      password: "123123"
+    });
+
+    const response = await request(app)
+      .get("/dashboard")
+      .set("Authorization", `Bearer ${user.generateToken()}`);
+
+    expect(response.status).toBe(200);
+  });
+
+  it("should not be able to acess private routes when not authenticated", async () => {
+    const response = await request(app).get("/dashboard");
+
+    expect(response.status).toBe(401);
+  });
 });
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,8 @@ const { User } = require("./app/models");
 const SessionController = require("./app/controllers/SessionController");
 
 routes.post("/sessions", SessionController.store);
+routes.get("/dashboard", (req, res) => {
+  return res.status(200).send();
+});
 
 module.exports = routes;
